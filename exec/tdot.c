d90f463237b05c42225ab7a46a918d543fe04d13
David M. Rogers <predictivestatmech@gmail.com>
2015-06-24 15:41:43 -0400

    C99-ification - patch from Jeff Hammond

--- exec/tdot.c
+++ exec/tdot.c
@@ -51,21 +51,19 @@ void print_nvec(char *, int *x, int n);
  */
 
 void tensdot(const double beta,
-              double *C, int nc,
+              double * restrict C, int nc,
              const double alpha,
-              double *A, const int na, const int *sa, const uint8_t *pa,
-              double *B, const int nb, const int *sb, const uint8_t *pb) {
+              const double * restrict A, const int na, const int *sa, const uint8_t *pa,
+              const double * restrict B, const int nb, const int *sb, const uint8_t *pb) {
     if(na > 100 || nb > 100 || nc > na+nb || na < 1 || nb < 1 || nc < 0
             || (na+nb-nc)%2 == 1) {
         printf("whoa there!\n");
         return;
     }
 
-    double acc;
-    int i, j, k, I, J, K;
     int outer, inner;
-    int n = (na+nb-nc)/2;   // number of contraction indices (nc =na + nb - 2*n)
-    int ntot = na + nb - n; // number of "expanded output" indices
+    const int n = (na+nb-nc)/2;   // number of contraction indices (nc =na + nb - 2*n)
+    const int ntot = na + nb - n; // number of "expanded output" indices
                         // run-time index
     int *Aind = malloc(sizeof(int)*(2*(na+nb) + ntot + nc)); // na
                         // reverse permutations
@@ -89,11 +87,11 @@ void tensdot(const double beta,
                             // loop is rolled
 
     // Index pass 0 - check
-    for(i=0; i<ntot; i++) sc[i] = -1;
+    for(int i=0; i<ntot; i++) sc[i] = -1;
 
     // Index pass 1 - assign c-size, reverse permutations, and zero Aind/Bind
-    j=0; // index to aout / bout
-    for(i=0; i<na; i++) {
+    int j=0; // index to aout / bout
+    for(int i=0; i<na; i++) {
         if(pa[i] < 0 || pa[i] >= ntot) {
             printf("A index out of range (%d)\n", pa[i]);
             free(Aind);
@@ -114,7 +112,7 @@ void tensdot(const double beta,
         Aind[i] = 0;
     }
     j=0; // index to aout / bout
-    for(i=0; i<nb; i++) {
+    for(int i=0; i<nb; i++) {
         if(pb[i] < 0 || pb[i] >= ntot) {
             printf("B index out of range (%d)\n", pb[i]);
             free(Aind);
@@ -154,10 +152,10 @@ void tensdot(const double beta,
         inner = szprod(inb,  n-1);
 
     // Index pass 2 - assign inner and outer boundaries from sc
-    for(i=0; i<na-n; i++) {
+    for(int i=0; i<na-n; i++) {
         Aoutb[i] = sc[pa[aout[i]]];
     }
-    for(i=0; i<nb-n; i++) {
+    for(int i=0; i<nb-n; i++) {
         Boutb[i] = sc[pb[bout[i]]];
     }
     //print_nvec("Aoutb =", Aoutb, na-n);
@@ -168,22 +166,22 @@ void tensdot(const double beta,
       int inner2 = sc[ntot-1]; // inner loop size (larger is better)
 
       // determine inner-loop step values (smaller is better)
-      for(i=ain[n-1]+1; i<na; i++) Aistep *= sa[i];
-      for(i=bin[n-1]+1; i<nb; i++) Bistep *= sb[i];
+      for(int i=ain[n-1]+1; i<na; i++) Aistep *= sa[i];
+      for(int i=bin[n-1]+1; i<nb; i++) Bistep *= sb[i];
 
-      for(I=0; I<outer; I++) {
-        acc = 0.0;
-        for(J=0; J<inner; J++) {
+      for(int I=0; I<outer; I++) {
+        double acc = 0.0;
+        for(int J=0; J<inner; J++) {
             // Track lowest index inner loop separately.
             // (and leave corresponding Aind / Bind at 0)
-            j = Aind[0];
-            for(i=1; i<na; i++) // Horner
+            int j = Aind[0];
+            for(int i=1; i<na; i++) // Horner
                 j = j*sa[i] + Aind[i];
-            k = Bind[0];
-            for(i=1; i<nb; i++) // Horner
+            int k = Bind[0];
+            for(int i=1; i<nb; i++) // Horner
                 k = k*sb[i] + Bind[i];
 
-            for(K=0; K<inner2; K++) {
+            for(int K=0; K<inner2; K++) {
                 //printf("  "); print_vec(Aind, na); printf(" = %d ", j);
                 //print_vec(Bind, nb); printf(" = %d\n", k);
                 acc += BINARY_OP(A[j], B[k]);
@@ -191,7 +189,7 @@ void tensdot(const double beta,
             }
 
             // ck what boundaries next J-step hits
-            for(i=0; i<n-1; i++) { // loop over inner indices
+            for(int i=0; i<n-1; i++) { // loop over inner indices
                 Aind[ain[i]] = (Aind[ain[i]] + !((J+1)%inb[i]))%sa[ain[i]];
                 Bind[bin[i]] = (Bind[bin[i]] + !((J+1)%inb[i]))%sb[bin[i]];
             }
@@ -200,29 +198,29 @@ void tensdot(const double beta,
         C[I] = alpha*acc + beta*C[I];
 
         // ck what boundaries next I-step hits
-        for(i=0; i<na-n; i++) { // outer indices assoc. to A
+        for(int i=0; i<na-n; i++) { // outer indices assoc. to A
             Aind[aout[i]] = (Aind[aout[i]] + !((I+1)%Aoutb[i]))%sa[aout[i]];
         }
-        for(i=0; i<nb-n; i++) { // outer indices assoc. to B
+        for(int i=0; i<nb-n; i++) { // outer indices assoc. to B
             Bind[bout[i]] = (Bind[bout[i]] + !((I+1)%Boutb[i]))%sb[bout[i]];
         }
       }
     } else { // lots of GER
-      for(I=0; I<outer; I++) {
-        j = Aind[0];
-        for(i=1; i<na; i++) // Horner
+      for(int I=0; I<outer; I++) {
+        int j = Aind[0];
+        for(int i=1; i<na; i++) // Horner
             j = j*sa[i] + Aind[i];
-        k = Bind[0];
-        for(i=1; i<nb; i++) // Horner
+        int k = Bind[0];
+        for(int i=1; i<nb; i++) // Horner
             k = k*sb[i] + Bind[i];
 
         C[I] = alpha*BINARY_OP(A[j], B[k]) + beta*C[I];
 
         // ck what boundaries next I-step hits
-        for(i=0; i<na-n; i++) { // outer indices assoc. to A
+        for(int i=0; i<na-n; i++) { // outer indices assoc. to A
             Aind[aout[i]] = (Aind[aout[i]] + !((I+1)%Aoutb[i]))%sa[aout[i]];
         }
-        for(i=0; i<nb-n; i++) { // outer indices assoc. to B
+        for(int i=0; i<nb-n; i++) { // outer indices assoc. to B
             Bind[bout[i]] = (Bind[bout[i]] + !((I+1)%Boutb[i]))%sb[bout[i]];
         }
       }
@@ -232,10 +230,10 @@ void tensdot(const double beta,
 
 // Form strides from lengths of dimensions.
 int szprod(int *x, int n) {
-    int i, j, k = 1; // last stride is 1 (C-ordering)
+    int k = 1; // last stride is 1 (C-ordering)
 
-    for(i=n-1; i>=0; i--) {
-        j = x[i];
+    for(int i=n-1; i>=0; i--) {
+        int j = x[i];
         x[i] = k;
         k *= j;
     }
@@ -243,7 +241,6 @@ int szprod(int *x, int n) {
 }
 
 void print_nvec(char *str, int *x, int n) {
-    int i;
     if(n == 0) {
         printf("%s ()\n", str);
         return;
@@ -251,14 +248,13 @@ void print_nvec(char *str, int *x, int n) {
         printf("%s (", str);
     }
 
-    for(i=0; i<n-1; i++) {
+    for(int i=0; i<n-1; i++) {
         printf("%d, ", x[i]);
     }
-    printf("%d)\n", x[i]);
+    printf("%d)\n", x[n-1]);
 }
 
 void print_vec(int *x, int n) {
-    int i;
     if(n == 0) {
         printf("()");
         return;
@@ -266,8 +262,8 @@ void print_vec(int *x, int n) {
         printf("(");
     }
 
-    for(i=0; i<n-1; i++) {
+    for(int i=0; i<n-1; i++) {
         printf("%d, ", x[i]);
     }
-    printf("%d)", x[i]);
+    printf("%d)", x[n-1]);
 }
